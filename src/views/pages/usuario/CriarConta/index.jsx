fb8a6b173bfa217360a2e45e6428f513b6ad8eba
Geverson <geversonjosedesouza@gmail.com>
2022-08-29 00:47:08 -0300

    Layout finalizado e início de xomunicação com API

--- src/views/pages/usuario/CriarConta/index.jsx
+++ src/views/pages/usuario/CriarConta/index.jsx
@@ -7,99 +7,96 @@ import AlertaErro from '../../../components/AlertaErro';
 import AlertaAtencao from '../../../components/AlertaAtencao';
 import AlertaSucesso from '../../../components/AlertaSucesso';
 import BotaoConfirmar from '../../../components/BotaoConfirmar';
+import { recuperarSenha } from '../../../../services/usuario'
+//Teste
+import { authorizationServerRecuperarSenha } from '../../../../core/api';
 
 const CriarConta = (props) => {
-    const [id, setId] = useState("");
-    const [senha, setSenha] = useState("");
-    const [recuperarSenha, setRecuperarSenha] = useState("");
-    const [erro, setErro] = useState("");
-    const [sucesso, setSucesso] = useState("");
-    const [atencao, setAtencao] = useState("");
+    const [nome, setNome] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [erro, setErro] = useState(null);
+    const [sucesso, setSucesso] = useState(null);
+    const [atencao, setAtencao] = useState(null);
     const [aguardando, setAguardando] = useState(false);
-    const [recuperarSenhaInvalida, setRecuperarSenhaInvalida] = useState(false);
     const [formularioSucesso, setFormularioSucesso] = useState(false);
 
     const criticas = () => {
-        if (!senha) return setAtencao({ mensagem: "Preencha o campo senha!" });
-        if (senha.length < 6) return setAtencao({ mensagem: "A senha precisa ter pelo menos seis caracteres!" });
+        if (!nome) return setAtencao({ mensagem: "Preencha o campo nome!" });
+        if (!email) return setAtencao({ mensagem: "Preencha o campo e-mail!" });
         return true;
     }
 
-    const atualizarSenha = (e) => {
+    const cadastrarSemSenha = async (e) => {
         e.preventDefault();
         if (!criticas()) return;
         setAguardando(true);
-        props.alterarSenha({ id, recuperarSenha, senha }, (retorno) => {
-            if (retorno.erro.erro) {
+        const dados = { nome, email };
+        await recuperarSenha(dados, (callback) => {
+            console.log(callback);
+            if (callback.erro.erro) {
                 setSucesso("");
-                setErro({ mensagem: retorno.erro.mensagem });
+                setErro({ mensagem: callback.erro.mensagem });
             } else {
                 setErro("");
-                setSucesso({ mensagem: "Senha alterada com sucesso!" });
+                setSucesso({ mensagem: "Cadastro realizado com sucesso, você receberá um email com código de acesso para criar asenha!" });
                 setFormularioSucesso(true);
             }
         });
+        //Teste
+        authorizationServerRecuperarSenha()
+            .post('/usuarios/sem-senha', dados)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+
+        setAguardando(false);
     }
 
     //Ao renderizar o componente
     useEffect(() => {
         setAguardando(true);
-        if (props.match)
-            props.validacaoRecuperarSenha(props.match.params.recuperarSenha, (retorno) => {
-                if (retorno.erro.erro) {
-                    setAtencao({ mensagem: 'Senha provisória inválida, solicite novo link para atualizar senha!' })
-                    setRecuperarSenha(true);
-                    setRecuperarSenhaInvalida(true);
-                    setAguardando(false);
-                } else {
-                    setId(retorno.erro.usuario._id);
-                    setRecuperarSenha(props.match.params.recuperarSenha);
-                    setAguardando(false);
-                }
-            })
+        if (props.params)
+            setEmail(props.params.email);
+        setAguardando(false);
         // eslint-disable-next-line
     }, [])
 
-    if (recuperarSenhaInvalida)
-        return <Navigate to='/' state={atencao} replace />
-
     if (formularioSucesso)
-        return <Navigate to='/' state={sucesso} replace />
+        return <Navigate to='/sgu_web/validar-acesso' state={sucesso} replace />
 
     return (
         <div className="container-login">
             <div className="login card shadow">
-                <Form onSubmit={atualizarSenha} className="form-signin text-center">
-                    <img className="mb-4" src={logo_sistema} alt="Celke" width="72" height="72" />
-                    <h1 className="h3 mb-3 font-weight-normal">Alterar Senha</h1>
+                <Form onSubmit={cadastrarSemSenha} className="form-signin text-center">
+                    <img className="mb-4" src={logo_sistema} alt="logo" width="72" height="72" />
+                    <h1 className="h3 mb-3 font-weight-normal">Cadastre-se</h1>
                     <AlertaErro erro={erro} />
                     <AlertaAtencao atencao={atencao} />
                     <AlertaSucesso sucesso={sucesso} />
-                    <Input
-                        type="hidden"
-                        value={id}
-                        name="id"
-                        id="id" />
-                    <Input
-                        type="hidden"
-                        value={recuperarSenha}
-                        name="recuperarSenha"
-                        id="recuperarSenha" />
                     <FormGroup>
                         <InputGroup>
-                            <InputGroupText style={{ height: 46 }}>Senha</InputGroupText>
+                            <InputGroupText style={{ height: 46 }}>Nome/Apelido</InputGroupText>
                             <Input
-                                type="password"
-                                value={senha}
-                                name="senha"
-                                id="senha"
-                                placeholder="Digite a nova senha do usuário"
-                                onChange={(ev) => setSenha(ev.target.value)} required />
+                                type="text"
+                                value={nome}
+                                name="nome"
+                                id="nome"
+                                placeholder="Digite nome/apelido"
+                                onChange={(ev) => setNome(ev.target.value)} required />
                         </InputGroup>
                     </FormGroup>
-
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroupText style={{ height: 46 }}>Em-mail</InputGroupText>
+                            <Input
+                                type="email"
+                                value={email}
+                                name="email"
+                                id="email"
+                                placeholder="Digite o e-mail"
+                                onChange={(ev) => setEmail(ev.target.value)} required />
+                        </InputGroup>
+                    </FormGroup>
                     <BotaoConfirmar aguardando={aguardando} />
-
                     <p className="text-center mt-2">
                         <Link to='/sgu_web/' className='remove-sublinhado'>Login</Link>
                     </p>
