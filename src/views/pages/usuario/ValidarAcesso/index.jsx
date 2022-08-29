267388575adc5cc802ca6483c32b3c31bf9b47e6
Geverson <geversonjosedesouza@gmail.com>
2022-08-29 01:00:17 -0300

    Conslusão do layout de validação de acesso

--- src/views/pages/usuario/ValidarAcesso/index.jsx
+++ src/views/pages/usuario/ValidarAcesso/index.jsx
@@ -8,12 +8,13 @@ import AlertaSucesso from '../../../components/AlertaSucesso';
 import BotaoLogin from '../../../components/BotaoLogin';
 
 const Login = (props) => {
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [senhaConfirma, setSenhaConfirma] = useState("");
-    const [atencao, setAtencao] = useState("");
-    const [sucesso, setSucesso] = useState("");
-    const [erro, setErro] = useState("");
+    const [codigo, setCodigo] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [senha, setSenha] = useState(null);
+    const [senhaConfirma, setSenhaConfirma] = useState(null);
+    const [atencao, setAtencao] = useState(null);
+    const [sucesso, setSucesso] = useState(null);
+    const [erro, setErro] = useState(null);
     const [aguardando, setAguardando] = useState(false);
 
     //Ao renderizar componente
@@ -21,13 +22,14 @@ const Login = (props) => {
         const { location } = props;
 
         if (location && location.state) {
+            setEmail(location.state.email)
             setSucesso(location.state.sucesso);
             setAtencao(location.state.atencao);
         }
         // eslint-disable-next-line
     }, []);
 
-    const login = (e) => {
+    const validarAcessoo = (e) => {
         e.preventDefault();
         setErro("");
         setAtencao("");
@@ -45,20 +47,35 @@ const Login = (props) => {
     }
 
     const criticas = () => {
+        if (!codigo) return setAtencao({ mensagem: "Preencha o campo código!" });
         if (!email) return setAtencao({ mensagem: "Preencha o campo e-mail!" });
         if (!senha) return setAtencao({ mensagem: "Preencha o campo senha!" });
+        if (!senhaConfirma) return setAtencao({ mensagem: "Preencha o campo confirma senha!" });
+        if (senha !== senhaConfirma) return setAtencao({ mensagem: "As senhas não são iguais!" });
         return true;
     }
 
     return (
         <div className="container-login">
             <div className="login card shadow">
-                <Form onSubmit={login} className="form-signin text-center">
+                <Form onSubmit={validarAcessoo} className="form-signin text-center">
                     <img className="mb-4" src={logo_sistema} alt="logo" width="72" height="72" />
                     <h1 className="h3 mb-3 font-weight-normal">Área Restrita</h1>
                     <AlertaErro erro={erro} />
                     <AlertaAtencao atencao={atencao} />
                     <AlertaSucesso sucesso={sucesso} />
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroupText>Código</InputGroupText>
+                            <Input
+                                type="text"
+                                value={codigo}
+                                name="codigo"
+                                id="codigo"
+                                placeholder="Código de validação"
+                                onChange={(ev) => setCodigo(ev.target.value)} required />
+                        </InputGroup>
+                    </FormGroup>
                     <FormGroup>
                         <InputGroup>
                             <InputGroupText>E-mail</InputGroupText>
@@ -85,7 +102,7 @@ const Login = (props) => {
                     </FormGroup>
                     <FormGroup>
                         <InputGroup>
-                            <InputGroupText style={{ height: 46 }}>Senha</InputGroupText>
+                            <InputGroupText style={{ height: 46 }}>Confirma senha</InputGroupText>
                             <Input
                                 type="password"
                                 value={senhaConfirma}
@@ -97,7 +114,9 @@ const Login = (props) => {
                     </FormGroup>
                     <BotaoLogin aguardando={aguardando} />
                     <p className="text-center mt-2">
-                        <Link to='/sgu_web/' className='remove-sublinhado'>Login</Link>
+                        <Link to='/sgu_web/criar-conta' className='remove-sublinhado'>Cadastrar</Link>
+                        {' - '}
+                        <Link to='/sgu_web/recuperar-senha' className='remove-sublinhado'>Esqueceu a senha?</Link>
                     </p>
                 </Form>
             </div>
