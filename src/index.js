de3309747eb4e6e17a4222e953b722da2bf2c3b7
Geverson <geversonjosedesouza@gmail.com>
2022-08-29 00:36:28 -0300

    Renomeado arquivo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Rotas from './core/Routes';
+import Rotas from './core/routes';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
