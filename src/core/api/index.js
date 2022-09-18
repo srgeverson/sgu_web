04fd528cdf8a2c1963b392ab6fe1ecfe2e0d60e9
Geverson <geversonjosedesouza@gmail.com>
2022-09-18 18:35:36 -0300

    Configurações temporárias para deploy

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { encode } from 'base-64';
 
-const client_id = 'api_node';
-const client_secret = '123321';
-const url_api = `http://localhost:8080/v1`;
+const client_id = process.env.CLIENT_ID ? process.env.CLIENT_ID : 'api_node';
+const client_secret = process.env.CLIENT_SECRET ? process.env.CLIENT_SECRET : '123321';
+const port = process.env.SERVER_PORT ? process.env.SERVER_PORT : 8181;
+const url = process.env.SERVER_URL ? process.env.SERVER_URL : `http://mystore-app.ddns.net`;
+const url_api = process.env.SERVER_URL ? process.env.SERVER_URL : `${url}:${port}/v1`;
 
 const api = (token) => {
     return axios.create({
--- src/core/config/index.js
+++ src/core/config/index.js
@@ -1,1 +1,1 @@
-export const publicURL = process.env.PUBLIC_URL && process.env.PUBLIC_URL;
+export const publicURL = process.env.PUBLIC_URL ? process.env.PUBLIC_URL: 'sgu_web';
