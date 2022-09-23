7d70c91a9cf58c6184b9415bf43e5e7cc9382c41
Geverson <geversonjosedesouza@gmail.com>
2022-09-23 19:04:51 -0300

    Correção de rota fixa

--- src/core/Routes/AuthorizeRoutes.js
+++ src/core/Routes/AuthorizeRoutes.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { Context } from '../context';
+import { publicURL } from '../config';
 
 const AuthorizeRoutes = () => {
     const { token } = useContext(Context);
     if (token)
         return <Outlet />
     else
-        return <Navigate to='/sgu_web/' state={{ erro: true, mensagem: 'Usuário não autenticado!' }} replace />
+        return <Navigate to={`${publicURL}/`} state={{ erro: true, mensagem: 'Usuário não autenticado!' }} replace />
 }
 
 export default AuthorizeRoutes;
