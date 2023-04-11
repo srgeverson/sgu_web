68b730798137f3afc682d703caf53f775ae8a381
geverson.souza <geverson.souza@odontosystem.com.br>
2023-04-11 04:55:52 -0300

    correção cliente secrete

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { encode } from 'base-64';
 
 const client_id = process.env.CLIENT_ID ? process.env.CLIENT_ID : 'api_node';
-const client_secret = process.env.CLIENT_SECRET ? process.env.CLIENT_SECRET : '123321';
+const client_secret = process.env.CLIENT_SECRET ? process.env.CLIENT_SECRET : '123456';
 const port = process.env.SERVER_PORT ? process.env.SERVER_PORT : 8181;
 const url = process.env.SERVER_URL ? process.env.SERVER_URL : `https://mystore-app.ddns.net`;
 const url_api = process.env.SERVER_URL ? process.env.SERVER_URL : `${url}:${port}/v1`;
